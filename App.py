2396ebcfc9366600c08dcfc8891e5d8462f8de83
Mashiro0128 <chaelong239@gmail.com>
2026-09-22 13:37:05 +0800

    Add welcome message and sidebar divider
    Added welcome message to the main page.

--- App.py
+++ App.py
@@ -4,8 +4,13 @@ import streamlit as st
 
 st.set_page_config(page_title="御風東都全球資訊網（測試版）")
 st.title("御風東都全球資訊網（測試版）")
-st.sidebar.divider()  # 加一條分隔線
 
+st.write("
+    廢土伺服器第九分流/n
+    御風東都社區/n
+    歡迎你
+         ")
+st.sidebar.divider()  # 加一條分隔線
 st.sidebar.header("網站分頁")
 # 初始化預設分頁
 if "page" not in st.session_state:
