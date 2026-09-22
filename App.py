2ca4c62f3784836418b2c7722a30b287ed6b0a62
Mashiro0128 <chaelong239@gmail.com>
2026-09-22 11:52:08 +0800

    Fix indentation for tab1 in App.py

--- App.py
+++ App.py
@@ -7,7 +7,6 @@ st.title("御風東都全球資訊網（測試版）")
 #st.header("")
 
 st.sidebar.header("網站分頁")
-tab1, tab2, tab3 = st.tabs(["📊 回報清查", "📖 使用說明", "✉️ 聯絡資訊"])
-
-with tab1:
-    st.title("政務院")
+    tab1, tab2, tab3 = st.tabs(["📊 回報清查", "📖 使用說明", "✉️ 聯絡資訊"])
+        with tab1:
+        st.title("政務院")
