f8a836101764fa76a1e87d465f5008e03340b3c3
Mashiro0128 <chaelong239@gmail.com>
2026-09-22 16:11:23 +0800

    Update App.py

--- App.py
+++ App.py
@@ -41,7 +41,10 @@ st.markdown(
 )
 
 # 2. 側邊欄抬頭資訊
-st.sidebar.image("螢幕擷取畫面 2025-09-18 171217")
+try:
+    st.sidebar.image("螢幕擷取畫面 2025-09-18 171217.png") 
+except:
+    pass
 st.sidebar.markdown("""
     廢土伺服器第九分流  
     **御風東都社區**  
