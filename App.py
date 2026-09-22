f593a5eee9d80040aaceb2d7ea315c4c0ac8f1b2
Mashiro0128 <chaelong239@gmail.com>
2026-09-22 11:40:49 +0800

    Initialize Streamlit app with basic layout

---
+++ App.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_cinfig(page_title="御風東都全球資訊網（測試版）")
+st.title("御風東都全球資訊網（測試版）")
+＃st.header("")
+
+st.sidebar.header("網站分頁")
+sider_text = st.sidebar.text_area("御風東都行政機關")
+
+tab1, tab2, tab3 = st.tabs(["📊 回報清查", "📖 使用說明", "✉️ 聯絡資訊"])
+
+with tab1
+    st.title("政務院")
